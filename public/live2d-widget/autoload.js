b554cf0f3a9bf1b79c779afd08e0afb568e61fc7
origamiwang <2241375541@qq.com>
2023-04-22 14:43:23 +0800

    try again

--- public/live2d-widget/autoload.js
+++ public/live2d-widget/autoload.js
@@ -1,7 +1,7 @@
 // live2d_path 参数建议使用绝对路径
 // const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 // const live2d_path = "http://124.71.22.168/live2d-widget/";
-const live2d_path = "./";
+const live2d_path = "./public/live2d-widget/";
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
